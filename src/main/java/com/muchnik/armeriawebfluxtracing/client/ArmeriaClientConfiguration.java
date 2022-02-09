9136e83e4a5cfa935ee1bb79ca73cd9781c43a67
Muchnik Andrey <amuchnik@alfabank.ru>
2022-02-09 15:52:06 +0300

    Added armeria brave Added commented not working solution

--- src/main/java/com/muchnik/armeriawebfluxtracing/client/ArmeriaClientConfiguration.java
+++ src/main/java/com/muchnik/armeriawebfluxtracing/client/ArmeriaClientConfiguration.java
@@ -1,7 +1,11 @@
 package com.muchnik.armeriawebfluxtracing.client;
 
 import brave.Tracing;
+import brave.context.slf4j.MDCScopeDecorator;
+import brave.propagation.CurrentTraceContext;
 import com.linecorp.armeria.client.Clients;
+import com.linecorp.armeria.client.brave.BraveClient;
+import com.linecorp.armeria.common.brave.RequestContextCurrentTraceContext;
 import com.linecorp.armeria.common.logging.RequestLogProperty;
 import com.muchnik.armeriawebfluxtracing.iface.MultiplicationService;
 import lombok.extern.slf4j.Slf4j;
@@ -12,9 +16,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ArmeriaClientConfiguration {
 
+    // private static final CurrentTraceContext traceCtx =
+    //         RequestContextCurrentTraceContext.builder()
+    //                                          .addScopeDecorator(MDCScopeDecorator.get())
+    //                                          .build();
+    // private static final Tracing tracing = Tracing.newBuilder()
+    //                                               .currentTraceContext(traceCtx)
+    //                                               .build();
+
     @Bean
     public MultiplicationService.AsyncIface multiplicationServiceClient() {
         return Clients.builder("tbinary+http://localhost:9090/multiply")
+                      //  BraveClient decorator not working properly with BraveAutoConfiguration#tracing()
+                      //  Which is based on ThreadLocalCurrentTraceContext
+                      //  .decorator(BraveClient.newDecorator(tracing))
                       .decorator((delegate, ctx, req) -> {
                           String traceId = Tracing.current().currentTraceContext().get().traceIdString();
                           ctx.log()
